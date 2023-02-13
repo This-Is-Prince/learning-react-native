d39ae3b954002b3f3bdcd3570f4b0a1ed4681634
This-Is-Prince <this.is.kumar.prince@gmail.com>
2023-02-13 06:44:03 +0530

    feat(FlatCards): flat cards added

--- styler/components/FlatCards.tsx
+++ styler/components/FlatCards.tsx
@@ -1,12 +1,81 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {ScrollView, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 
 export default function FlatCards() {
   return (
     <View>
-      <Text>FlatCards</Text>
+      <Text style={styles.headingText}>Flat Cards</Text>
+      <ScrollView horizontal={true}>
+        <View style={styles.container}>
+          <View style={[styles.card, styles.cardOne]}>
+            <Text>Red</Text>
+          </View>
+          <View style={[styles.card, styles.cardTwo]}>
+            <Text>Green</Text>
+          </View>
+          <View style={[styles.card, styles.cardThree]}>
+            <Text>Blue</Text>
+          </View>
+          <View style={[styles.card, styles.cardOne]}>
+            <Text>Red</Text>
+          </View>
+          <View style={[styles.card, styles.cardTwo]}>
+            <Text>Green</Text>
+          </View>
+          <View style={[styles.card, styles.cardThree]}>
+            <Text>Blue</Text>
+          </View>
+          <View style={[styles.card, styles.cardOne]}>
+            <Text>Red</Text>
+          </View>
+          <View style={[styles.card, styles.cardTwo]}>
+            <Text>Green</Text>
+          </View>
+          <View style={[styles.card, styles.cardThree]}>
+            <Text>Blue</Text>
+          </View>
+          <View style={[styles.card, styles.cardOne]}>
+            <Text>Red</Text>
+          </View>
+          <View style={[styles.card, styles.cardTwo]}>
+            <Text>Green</Text>
+          </View>
+          <View style={[styles.card, styles.cardThree]}>
+            <Text>Blue</Text>
+          </View>
+        </View>
+      </ScrollView>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  headingText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    paddingHorizontal: 8,
+  },
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    gap: 8,
+    padding: 8,
+  },
+  card: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 100,
+    height: 100,
+    borderRadius: 4,
+  },
+  cardOne: {
+    backgroundColor: '#EF5354',
+  },
+  cardTwo: {
+    backgroundColor: '#50DBB4',
+  },
+  cardThree: {
+    backgroundColor: '#5DA3FA',
+  },
+});
