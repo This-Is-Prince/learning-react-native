1e3f6e861977a741bc47ac9f2267e9c1cd2821a9
This-Is-Prince <this.is.kumar.prince@gmail.com>
2023-02-14 09:19:51 +0530

    feat: scrollview

---
+++ styler/components/ElevatedCards.tsx
@@ -0,0 +1,61 @@
+import {ScrollView, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const ElevatedCards = () => {
+  return (
+    <View>
+      <Text style={styles.headingText}>ElevatedCards</Text>
+      <ScrollView horizontal={true} style={styles.container}>
+        <View style={[styles.card, styles.cardElevated]}>
+          <Text>Tap</Text>
+        </View>
+        <View style={[styles.card, styles.cardElevated]}>
+          <Text>me</Text>
+        </View>
+        <View style={[styles.card, styles.cardElevated]}>
+          <Text>to</Text>
+        </View>
+        <View style={[styles.card, styles.cardElevated]}>
+          <Text>Scroll</Text>
+        </View>
+        <View style={[styles.card, styles.cardElevated]}>
+          <Text>more....</Text>
+        </View>
+        <View style={[styles.card, styles.cardElevated]}>
+          <Text>🫢</Text>
+        </View>
+      </ScrollView>
+    </View>
+  );
+};
+
+export default ElevatedCards;
+
+const styles = StyleSheet.create({
+  headingText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    paddingHorizontal: 8,
+  },
+  container: {},
+  card: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 100,
+    height: 100,
+    borderRadius: 4,
+    margin: 8,
+  },
+  cardElevated: {
+    backgroundColor: '#CAD5E2',
+    elevation: 4,
+    shadowOffset: {
+      height: 1,
+      width: 1,
+    },
+    shadowColor: '#333',
+    shadowOpacity: 0.4,
+    shadowRadius: 2,
+  },
+});
