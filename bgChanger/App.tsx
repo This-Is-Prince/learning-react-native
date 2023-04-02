97adddb177207fa4e48ac3d66cd9673dd6419e40
This-Is-Prince <this.is.kumar.prince@gmail.com>
2023-04-02 17:49:33 +0530

    feat: bgChanger app complete

---
+++ bgChanger/App.tsx
@@ -0,0 +1,55 @@
+import {
+  StyleSheet,
+  StatusBar,
+  TouchableOpacity,
+  Text,
+  View,
+} from 'react-native';
+import React, {FC, useState} from 'react';
+
+const App: FC = () => {
+  const [randomBackground, setRandomBackground] = useState('#FFF');
+
+  const generateColor = () => {
+    const hexRange = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += hexRange[Math.floor(Math.random() * 16)];
+    }
+    setRandomBackground(color);
+  };
+
+  return (
+    <>
+      <StatusBar backgroundColor={'#000000'} />
+      <View style={[styles.container, {backgroundColor: randomBackground}]}>
+        <TouchableOpacity onPress={generateColor}>
+          <View style={styles.actionBtn}>
+            <Text style={styles.actionBtnTxt}>Press me</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+    </>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  actionBtn: {
+    borderRadius: 12,
+    backgroundColor: '#6A1B4D',
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+  },
+  actionBtnTxt: {
+    fontSize: 24,
+    color: '#FFF',
+    textTransform: 'uppercase',
+  },
+});
